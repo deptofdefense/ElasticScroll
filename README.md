196f9fb8174b0ed8e5a75578281c27f5e6a996d5
Isaac Sears <isears@friends.dds.mil>
2018-03-29 11:18:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Elastic Scroll
+
+A minimal library for efficient elasticsearch queries
+
+## Install
+
+```bash
+git clone https://github.com/departmentofdefense/elasticscroll
+cd elasticscroll
+pip3 install .
+```
+
+## Usage
+
+```
+import elasticscroll
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='elasticscroll',
+    version='0.1',
+    description='minimal library for efficient elasticsearch queries',
+    url='https://github.com/departmentofdefense/ElasticScroll',
+    author='Isaac Sears',
+    author_email='isears@friends.dds.mil',
+    license='MIT',
+    packages=['elasticscroll'],
+    zip_safe=False
+)
